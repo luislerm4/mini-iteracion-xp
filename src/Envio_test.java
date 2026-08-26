a6ca99af72a10b210f4077d0244f9ab7d680bd7c
Luis Angel Lerma Coss <angelerma680@gmail.com>
2026-08-26 12:40:43 -0700

    Agregar casos de prueba, limites frontera y metodo verificar en Envio_test

--- src/Envio_test.java
+++ src/Envio_test.java
@@ -1,3 +1,24 @@
 public class Envio_test {
     public static void main(String[] args) {
+        //casos de prueba
+        verificar(100.0, Envio.calcularCostoEnvio(400.0), "Pedido $400 - Envio $100");
+        verificar(50.0, Envio.calcularCostoEnvio(700.0), "Pedido $700 - Envio 50$");
+        verificar(0.0, Envio.calcularCostoEnvio(1200.0), "Pedido $1200 - Envio 0$");
+
+        //casos frontera
+        verificar (100.0, Envio.calcularCostoEnvio(499.99), "Frontera $499.99 - Envio 100$");
+        verificar(50.0, Envio.calcularCostoEnvio(500.00), "Frontera $500.00 - Envio $50");
+        verificar(50.0, Envio.calcularCostoEnvio(999.99), "Frontera $999.99 - Envio $50");
+        verificar(0.0, Envio.calcularCostoEnvio(1000.00), "Frontera $1000.00 - Envio $0");
+
+        //Metodo para verificar resultados
+    }
+    public static void verificar (double esperado, double obtenido, String nombrePrueba) {
+            if (Math.abs(esperado - obtenido) < 0.001){
+                System.out.println("Paso: " + nombrePrueba);
+            } else {
+                System.err.println("fallo: " +nombrePrueba + ", Esperado: " + esperado +", Obtenido: " + obtenido);
+            }
+        }
+
     }
