9127efec34850c2f8cd2fcd39a34e4749658a42d
Luis Angel Lerma Coss <angelerma680@gmail.com>
2026-08-26 13:04:14 -0700

    Agregar soporte para clientes premium y pruebas

--- src/Envio.java
+++ src/Envio.java
@@ -1,8 +1,14 @@
 public class Envio {
-    public static double calcularCostoEnvio(double monto){
-        if (monto>500) {
+    public static double calcularCostoEnvio(double monto) {
+        return calcularCostoEnvio(monto, false);
+    }
+    public static double calcularCostoEnvio(double monto, boolean esPremium) {
+        if (esPremium) {
+            return 0.0;
+        }
+        if (monto < 500) {
             return 100.0;
-        }else if (monto <1000.0) {
+        } else if (monto < 1000.0) {
             return 50.0;
         } else {
             return 0.0;
--- src/Envio_test.java
+++ src/Envio_test.java
@@ -11,8 +11,12 @@ public class Envio_test {
         verificar(50.0, Envio.calcularCostoEnvio(999.99), "Frontera $999.99 - Envio $50");
         verificar(0.0, Envio.calcularCostoEnvio(1000.00), "Frontera $1000.00 - Envio $0");
 
-        //Metodo para verificar resultados
+        //cliente premium
+        verificar(0.0, Envio.calcularCostoEnvio(400.0, true), "Cliente Premium $400 - Envio $0");
+        verificar(0.0, Envio.calcularCostoEnvio(499.99, true), "Cliente Premium $499.99 - Envio $0");
+
     }
+    //Metodo para verificar resultados
     public static void verificar (double esperado, double obtenido, String nombrePrueba) {
             if (Math.abs(esperado - obtenido) < 0.001){
                 System.out.println("Paso: " + nombrePrueba);
